bd3943569fae25480e7f0c5748fa74802cb0d001
haein Oh <badarangdev@gmail.com>
2023-08-15 23:39:38 +0900

    feat: Add stage system

---
+++ Assets/badarang/Scripts/HandleFlag.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandleFlag : MonoBehaviour
+{
+    public Animator animator;
+    Rigidbody2D rb;
+    [SerializeField] private GameObject fireWorkParticle = default;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        animator.SetTrigger("Flag");
+        StartCoroutine("Particle");
+    }
+    
+    IEnumerator Particle()
+    {
+        yield return new WaitForSeconds(1.0f);
+        createFireWork();
+        yield return new WaitForSeconds(1.0f);
+        createFireWork();
+        yield return new WaitForSeconds(1.0f);
+        createFireWork();
+        yield return new WaitForSeconds(1.0f);
+        GameObject obj = GameObject.Find("GameManager");
+        obj.GetComponent<GoToLobby>().goLobbyScene();
+    }
+
+    void createFireWork()
+    {
+        float randomX = Random.Range(-1, 1);
+        float randomY = Random.Range(0, 1);
+        float size = 5.0f;
+        GameObject particle = Instantiate(fireWorkParticle, rb.transform.position + new Vector3(randomX * size, randomY * size, 0f), transform.rotation);
+        ParticleSystem particlesys = particle.GetComponent<ParticleSystem>();
+        particlesys.Play();
+    }
+}
